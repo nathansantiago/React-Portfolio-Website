39457ed8fb5474ee01fe76a78985a9542ceeee3d
Nathan Santiago <santiagonathan9254@gmail.com>
2024-02-11 17:48:00 -0500

    Create contact form.

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Layout from './Components/Layout'
 import Home from './Components/Home';
 import About from './Components/About'
+import Contact from './Components/Contact';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path='contact' element={<Contact />} />
         </Route>
       </Routes>
     </>
