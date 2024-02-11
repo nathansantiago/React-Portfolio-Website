baa1151dd8ce3034c08e25f26409715adb487958
Nathan Santiago <santiagonathan9254@gmail.com>
2024-02-11 18:25:13 -0500

    Finish website

--- react-portfolio/src/Components/Contact/index.js
+++ react-portfolio/src/Components/Contact/index.js
@@ -1,57 +1,111 @@
-import Loader from 'react-loaders'
-import './index.scss'
-import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
 
 const Contact = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const form = useRef()
 
-    useEffect(() => {
-        setTimeout(() => {
-          setLetterClass('text-animate-hover')
-        }, 3000)
-    }, [])
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
 
-    return (
-        <>
-        <div className='container contact-page'>
-           <div className='text-zone'>
-                <h1>
-                    <AnimatedLetters 
-                        letterClass={letterClass}
-                        strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
-                        idx={15}
-                    />
-                </h1>
-                <p>
-                    paragaph1
-                </p>
-                <div className='contact-form'>
-                    <form>
-                        <ul>
-                            <li className='half'>
-                                <input type='text' name='name' placeholder='Name' required/>
-                            </li>
-                            <li className='half'>
-                                <input type='email' name='email' placeholder='Email' required/>
-                            </li>
-                            <li>
-                                <input placeholder='Subject' type='text' name='subject' required/>
-                            </li>
-                            <li>
-                                <textarea placeholder='Message' name='message' required></textarea>
-                            </li>
-                            <li>
-                                <input type='submit' className='falt-button' value="SEND" />
-                            </li>
-                        </ul>
-                    </form>
-                </div>
-            </div> 
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm('gmail', 'template_YeJhZkgb', form.current, 'your-token')
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again')
+        }
+      )
+  }
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am interested in freelance opportunities - especially on ambitious
+            or large projects. However, if you have any other requests or
+            questions, don't hesitate to contact me using below form either.
+          </p>
+          <div className="contact-form">
+            <form ref={form} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input placeholder="Name" type="text" name="name" required />
+                </li>
+                <li className="half">
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
         </div>
-        <Loader type='pacman' />
-        </>
-    )
+        {/* <div className="info-map">
+          Nathan Santiago,
+          <br />
+          United States,
+          <br />
+          205 E Cameron Ave <br />
+          Chapel Hill, NC <br />
+          <br />
+          <span>freelancerslobodan@gmail.com</span>
+        </div> */}
+        <div className="map-wrap">
+          <MapContainer center={[35.912056, -79.051250]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[35.912056, -79.051250]}>
+              <Popup>Old Well</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
 }
 
 export default Contact
