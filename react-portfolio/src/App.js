747fbf5c65e84670d0108babb68604e0cb70828a
Nathan Santiago <santiagonathan9254@gmail.com>
2024-02-11 17:00:28 -0500

    Create home and contact page.

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,12 +1,17 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom'
 import Layout from './Components/Layout'
+import Home from './Components/Home';
+import About from './Components/About'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
       </Routes>
     </>
   );
